e8e2c6e2e389ea2168e72a60b053f3ea0248b237
MruthulManohar <mruthul.manohar@mavapartners.com>
2017-01-09 11:48:12 +0530

    Commit

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS_Class1
+{
+    public class Class1
+    {
+        int a;
+        int b;
+        int c;
+
+    }
+
+}
---
+++ HRMS_Class1/HRMS_Class1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS_Class1
+{
+    public class Class1
+    {
+        int a;
+        int b;
+
+    }
+
+}
