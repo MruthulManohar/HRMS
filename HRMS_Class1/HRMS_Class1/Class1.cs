368cee025c9873d7554612fcc818fe2141d562a6
MruthulManohar <mruthul.manohar@mavapartners.com>
2017-01-09 11:57:10 +0530

    Update Class1.cs

--- HRMS_Class1/HRMS_Class1/Class1.cs
+++ HRMS_Class1/HRMS_Class1/Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,7 @@ namespace HRMS_Class1
     {
         int a;
         int b;
-
+        int c;
     }
 
 }
